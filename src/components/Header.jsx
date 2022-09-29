2f5785ec51082b064ad296a82e307e6359a63a49
unknown <doniaskima344@gmail.com>
2022-09-29 22:57:19 +0100

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,62 @@
 import { useEffect } from "react";
 import Magdo from "../assets/logo.jpg";
 import { FaUser } from "react-icons/fa";
-
+import { FaShoppingBag } from "react-icons/fa";
 
 export default function () {
     return (
         <>
             <div>
                 <nav>
-                    <img src={Magdo} alt="magdo" />
-                    <h1>Welcome to Magdo</h1>
+                    <div className="website">
+                        <img src={Magdo} alt="Magdo" />
+                        <h1>Welcome to Magdo</h1>
+                    </div>
+                    <ul className="desktop-list">
+                        <a href="#home">Home</a>
+                        <a href="#menu">Menu</a>
+                        <a href="#about">About</a>
+                        <a href="#contact">Contact</a>
+                    </ul>
+                    <ul className="mobile-list">
+                        <div>
+
+                        </div>
+                        <div className="list">
+                            <a href="#home">Home</a>
+                            <a href="#menu">Menu</a>
+                            <a href="#about">About</a>
+                            <a href="#contact">Contact</a>
+                        </div>
+                        <a href="/auth">
+                            <FaUser />
+                            <h3>
+                                Sign in
+                            </h3>
+                        </a>
+                    </ul>
+                    <div className="wrap">
+                        <div className="container">
+                            <a href="/auth">
+                                <FaUser />
+                                <h3>Sign in</h3>
+                            </a>
+                            <div>
+                                <FaShoppingBag className="icon" />
+                                <div className="num">
+                                    {
+                                       
+                                    }
+                                </div>
+                            </div>
+                        </div>
+                        <div className="bars">
+                            <div className="bar"></div>
+                            <div className="bar"></div>
+                            <div className="bar"></div>
+                        </div>
+                    </div>
                 </nav>
-            </div>
-            <ul className="list">
-                <a href="#home" >Home</a>
-                <a href="#menu" >Menu</a>
-                <a href="#about" >about</a>
-                <a href="#contact" >Contact</a>
-            </ul>
-            <ul className="mobile-list">
-                <div></div>
-                <div className="listMobile">
-                    <a href="#home">Home</a>
-                    <a href="#menu">Menu</a>
-                    <a href="#about">About</a>
-                    <a href="#contact">Contact</a>
-                </div>
-            </ul>
-            <div className="wrap">
-                <div className="container">
-                    <a href="/auth">
-                        <FaUser />
-                        <h3>Sign in</h3>
-                    </a>
-                </div>
             </div>
         </>
     )
