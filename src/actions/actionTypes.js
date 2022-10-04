089e695c5efac496fb4649610308f47d039fa863
unknown <doniaskima344@gmail.com>
2022-10-04 21:23:20 +0100

    actionTypes

---
+++ src/actions/actionTypes.js
@@ -0,0 +1,28 @@
+import * as actionTypes from "../constants/actionTypes.js";
+
+export function addToCart(item){
+  return{
+    type:actionTypes.ADD_TO_CART,
+    payload:item
+  }
+}
+
+export function removeFromCart(id){
+  return{
+    type:actionTypes.REMOVE_FROM_CART,
+    payload:id
+  }
+}
+
+export function increaseQuantity(id){
+  return{
+    type:actionTypes.INCREASE_QUANTITY,
+    payload:id
+  }
+}
+
+export function decreaseQuantity(id){
+  return{
+    type:actionTypes.DECREASE_QUANTITY,
+    payload:id
+  }
