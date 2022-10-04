9266a37c7e71e2e461f39d3fd232af85bb2d6d0a
unknown <doniaskima344@gmail.com>
2022-10-04 23:21:53 +0100

    #CartItem added

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -3,9 +3,39 @@ import { FaTrash } from "react-icons/fa";
 import { useDispatch } from "react-redux";
 import { removeFromCart } from "../app/actions/cartActions.js";
 import { increaseQuantity , decreaseQuantity } from "../app/actions/cartActions.js";
-const CartItem = () => {
+
+const CartItem = ({name,id,picture,quantity}) => {
+
+    function remove(){
+        dispatch(removeFromCart(id));
+      }
+    
+      function increase(){
+        dispatch(increaseQuantity(id));
+      }
+    
+      function decrease(){
+        dispatch(decreaseQuantity(id));
+      }
+
   return (
- 
+    <>
+        <div className="item">
+        <div className="left">
+          <img src={picture} alt="cart item" />
+          <h3>{name}</h3>
+        </div>
+        <div className="right">
+          <div className="actions">
+            <span className="minus" onClick={decrease}>-</span>
+            <span className="number">{quantity}</span>
+            <span className="plus" onClick={increase}>+</span>
+          </div>
+          <h4>{price * quantity} $</h4>
+          <FaTrash className="trash" onClick={remove}/>
+        </div>
+        </div>
+    </>
   )
 }
 
