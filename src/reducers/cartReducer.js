eda8e6fd28495dec17e7e3a0108cacd42c76c1f2
unknown <doniaskima344@gmail.com>
2022-10-04 22:02:15 +0100

    #cartReducer

--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -6,3 +6,21 @@ const initialState = {
     quantity:0,
     total:0
   }
+
+export default function reducer(state=initialState, action){
+    switch(action.type){
+        case actionTypes.ADD_TO_CART:
+            return produce(state,(draft)=>{
+                const item= action.payload;
+                const exist= draft.items.find((x)=>x.id===item.id);
+                if(exist){
+                    exist.quantity++;
+                }else{
+                    draft.items.push(item);
+                }
+                draft.quantity++;
+                draft.total+=item.price;        
+            })
+    }
+
+}
