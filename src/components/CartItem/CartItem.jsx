ad423b6a2ddb52a2e8795b452af654d23ed12f25
unknown <doniaskima344@gmail.com>
2022-10-04 23:15:45 +0100

    #imports

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
- 
- 
-
+import { FaTrash } from "react-icons/fa";
+import { useDispatch } from "react-redux";
+import { removeFromCart } from "../app/actions/cartActions.js";
+import { increaseQuantity , decreaseQuantity } from "../app/actions/cartActions.js";
 const CartItem = () => {
   return (
  
