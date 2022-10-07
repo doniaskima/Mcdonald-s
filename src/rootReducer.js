e7f0aa8022eb5eaf7ff55a8acc5d2fc2014e862f
unknown <doniaskima344@gmail.com>
2022-10-07 14:49:23 +0100

    something is wrong

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -2,7 +2,7 @@ import { combineReducers } from "redux";
 import cartReducer from "./reducers/cartReducer.js";
 
 const rootReducer = combineReducers({
-  cart:cartReducer
+    cart: cartReducer
 })
 
 export default rootReducer;
